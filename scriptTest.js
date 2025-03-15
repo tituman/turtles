a89d4b1afe2f0ea54c8f164bd6d9bfc6f41530d8
dan <dan@dan.com>
2025-03-15 22:41:48 +0900

    can snap to grid, must optimize

--- scriptTest.js
+++ scriptTest.js
@@ -1,129 +1,178 @@
 // adapted from http://stackoverflow.com/questions/12786797/how-to-draw-rectangles-dynamically-in-svg
-svgns = "http://www.w3.org/2000/svg";
+const svgns = "http://www.w3.org/2000/svg";
+
 
 /******  preparations  *******/
-let SIDE = 20;//96;
-let sizeOfSVGinX = 900;
-let sizeOfSVGinY = 900;
+const SIDE = 40;//96;
+const sizeOfSVGinX = 900;
+const sizeOfSVGinY = 900;
 
 /******  constants  *******/
-let sqrt3 = Math.sqrt(3);
-let offsetfromZero = `translate(${1}, ${sqrt3 / 2})`;
-let myScale = `scale(${SIDE}, ${SIDE})`; //scale the whole svg to SIDE
-let gridStepsInX = 1.5; // from 1 hex to the next in X direction, steps always in 1.5
-let gridStepsInY = sqrt3/2; // from 1 hex to the next in Y direction
+const sqrt3 = Math.sqrt(3);
+const offsetfromZero = `translate(${1}, ${sqrt3 / 2})`;
+const myScale = `scale(${SIDE}, ${SIDE})`; //scale the whole svg to SIDE
+const gridStepsInX = 1.5; // from 1 hex to the next in X direction, steps always in 1.5
+const gridStepsInY = sqrt3 / 2; // from 1 hex to the next in Y direction
 
 //put SVG in the document
-let svg = createSvg();
+const svg = createSvg();
 // add background pattern to document
-let patt = createdefinesAndPattern();
+const patt = createdefinesAndPattern();
 //create hexes and kites for the pattern
 addKitesToPattern();
 addHexesToPattern();
 //make a shape with the pattern the size of the svg
-createRectforPattern();
+const rectBackground = createRectforPattern();
 
 // find center of center hex of pattern
-let centerOfCenterHex = findCenterOfCenterHex();
-let transCenter = `translate(${centerOfCenterHex.x}, ${centerOfCenterHex.y})`;
+const centerOfCenterHex = findCenterOfCenterHex();
+const transCenter = `translate(${centerOfCenterHex.x}, ${centerOfCenterHex.y})`;
 
 
 //add one turtle
+// //syntax: createTurtle(color, stepsInX, stepsInY, stepsIn60Deg, invert)
 let myTurt = createTurtle("blue");
-//myTurt.setAttributeNS(null, 'transform', `${transCenter} ${myScale}`);
 svg.appendChild(myTurt);
-
 //add another pink turtle
-
-//createTurtle(color, stepsInX, stepsInY, stepsIn60Deg, invert)
-myTurt = createTurtle(color="pink", stepsInX=2, stepsInY=0, stepsIn60Deg=1, invert=-1);
-//myTurt.setAttributeNS(null, 'transform', `${transCenter} ${myScale} ${myInvert} ${myTranslateArbitrary} ${myRotate} `);
+myTurt = createTurtle(color = "pink", stepsInX = 2, stepsInY = 0, stepsIn60Deg = 1, invert = -1);
 svg.appendChild(myTurt);
 
 /******** more testing */
 svg.addEventListener('mouseup', function (event) { evMouseUp(event) });
-let movingObject = null;
-var pt = svg.createSVGPoint();  // Created once for document
-let dragging = false;
-let offset={};
-let cursorpt={};
+svg.addEventListener('mousemove', function (event) { evMouseMove(event) });
+svg.addEventListener('mousedown', function (event) { evMouseDown(event) });
+
+var TransformRequestObj;
+var TransList;
+var DragTarget = null;
+var Dragging = false;
+var OffsetX = 0;
+var OffsetY = 0;
+
+
 function evMouseDown(e) {
-    pt.x = e.clientX;
-    pt.y = e.clientY;
-    dragging = true;
-    movingObject = e.srcElement;
+    if (!Dragging) //---prevents dragging conflicts on other draggable elements---
+    {
+        DragTarget = e.target;
+        if(DragTarget.id === "background") return;
+        //---reference point to its respective viewport--
+        let pnt = DragTarget.ownerSVGElement.createSVGPoint();
+        pnt.x = e.clientX;
+        pnt.y = e.clientY;
+        //---elements transformed and/or in different(svg) viewports---
+        let sCTM = DragTarget.getScreenCTM();
+        let Pnt = pnt.matrixTransform(sCTM.inverse());
 
-    // The cursor point, translated into svg coordinates
-    cursorpt =  pt.matrixTransform(svg.getScreenCTM().inverse());
-    console.log(`pt.x:${pt.x}, pt.y:${pt.y}`);
-    console.log("(cursorpt.x:" + cursorpt.x + ", cursorpt.y:" + cursorpt.y + ")");
+        TransformRequestObj = DragTarget.ownerSVGElement.createSVGTransform();
+        //---attach new or existing transform to element, init its transform list---
+        TransList = DragTarget.transform.baseVal;
 
-    
-  //offset = oMousePos(svg, e);
-    //console.log(e.srcElement.attributes.transform.nodeValue);
-    //e.srcElement.setAttributeNS(null, 'transform', `${ e.srcElement.attributes.transform.nodeValue} rotate(60,0,0)`);
-    //console.log(e.srcElement.attributes.transform.nodeValue);
-    //alert("click");
+        OffsetX = Pnt.x
+        OffsetY = Pnt.y
+
+        Dragging = true;
+    }
 }
 function evMouseMove(e) {
-    if (dragging) {
-        pt.x = e.clientX;
-        pt.y = e.clientY;
-        m = oMousePos(svg, e);
-        //var cursorpt = pt.matrixTransform(svg.getScreenCTM().inverse());
-        //console.log("(" + cursorpt.x + ", " + cursorpt.y + ")");
-        //let nextPos = findNextGridPosition(cursorpt.x, cursorpt.y);
-        
-        // e.srcElement.setAttributeNS(null, 'transform', `translate(${nextPos.x}, ${nextPos.y})`);
-        e.srcElement.setAttributeNS(null, 'transform', `translate(${m.x - offset.x}, ${m.y - offset.y})`);
-        console.log(`translate(${m.x - offset.x}, ${m.y - offset.y})`);
-    }else{
-        //console.log("moving");
+    if (Dragging) {
+        //var pnt = DragTarget.ownerSVGElement.createSVGPoint();
+        var pnt = svg.createSVGPoint();
+        let clientSVGOffset = svg.createSVGPoint();
+        let PntGrid = svg.createSVGPoint();
+        // cursor pointer in screen
+        pnt.x = e.clientX;
+        pnt.y = e.clientY;
+        console.log(`client x and y: ${pnt.x} , ${pnt.y}`);
+        //cusror pointer in svg coordinates
+        var PntSVG = pnt.matrixTransform(svg.getScreenCTM().inverse());
+        clientSVGOffset.x = pnt.x - PntSVG.x;
+        clientSVGOffset.y = pnt.y - PntSVG.y;
+        // grid posiition in screen coordinates (because added offset)
+        let posInGrid = findNextGridPosition(PntSVG.x, PntSVG.y);
+        PntGrid.x = posInGrid.x + clientSVGOffset.x ;
+        PntGrid.y = posInGrid.y + clientSVGOffset.y ;
+//Pnt.x -= OffsetX;
+//Pnt.y -= OffsetY;
+        console.log(`svg x and y: ${PntSVG.x} ,${PntSVG.y}`);
+        console.log(`next grid x and y: ${PntGrid.x} ,${PntGrid.y}`);
+        console.log(`client-svgOffset : ${clientSVGOffset.x} ,${clientSVGOffset.y}`);
+
+        //---elements in different(svg) viewports, and/or transformed ---
+        var Pnt = PntGrid.matrixTransform(DragTarget.getScreenCTM().inverse());        console.log(`will add this transform: ${Pnt.x} ,${Pnt.y}`);
+        TransformRequestObj.setTranslate(Pnt.x, Pnt.y)
+        TransList.appendItem(TransformRequestObj)
+        TransList.consolidate()
     }
 }
 function evMouseUp(e) {
-    // if (dragging) {
-    //     let nextPos = findNextGridPosition(cursorpt.x, cursorpt.y);
-    //     movingObject.setAttributeNS(null, 'transform', `translate(${nextPos.x}, ${nextPos.y})`);
-        
-    //     movingObject = null;
-    // }else{
-    //     console.log("up");
-    // }
-    // dragging = false;
-    myCirc = myCirc.cloneNode(true);
-    pt.x = e.clientX;
-    pt.y = e.clientY;
-    // The cursor point, translated into svg coordinates
-    cursorpt =  pt.matrixTransform(svg.getScreenCTM().inverse());
-    console.log("(cursorpt.x:" + cursorpt.x + ", cursorpt.y:" + cursorpt.y + ")");
-    let nextPos = findNextGridPosition(cursorpt.x, cursorpt.y);
-    myCirc.setAttributeNS(null, 'cx', nextPos.x);
-    myCirc.setAttributeNS(null, 'cy', nextPos.y);
-    svg.appendChild(myCirc);
+    if (!Dragging) return;
+
+    Dragging = false;
+return;
+    let pnt = DragTarget.ownerSVGElement.createSVGPoint();
+    pnt.x = 0;
+    pnt.y = 0;
+        console.log(`pntY: ${pnt.y}`);// pointer on screen
+    let cursorpt = pnt.matrixTransform(svg.getScreenCTM().inverse()); //pointer on svg
+        console.log(`cursorpnY: ${cursorpt.y}`);// pointer on svg --> make this snap to grid
+        console.log(`OffsetY: ${OffsetY}`);// Offset in x
+    //---elements in different(svg) viewports, and/or transformed ---
+    var Pnt = pnt.matrixTransform(DragTarget.getScreenCTM()); //0 of object relative to svg
+    //Pnt.x -= OffsetX;
+    //Pnt.y -= OffsetY;
+        console.log(`PntY 0 of object relative to svg: ${Pnt.y}`);//0 of object relative to svg
+    
+    let posInGrid = findNextGridPosition(Pnt.x, Pnt.y);
+    pnt.x = posInGrid.x;
+    pnt.y = posInGrid.y;
+        console.log(`posInGridY: ${posInGrid.y}`); //
+    Pnt = pnt.matrixTransform(DragTarget.getScreenCTM().inverse());
+    
+        console.log(`after inverse: ${Pnt.y}`); //
+    //TransformRequestObj.setTranslate(Pnt.x, Pnt.y);
+//TODO: get the orig turtle translations and use them instead of append to translist
+    let transSnap = `translate(${pnt.x},${pnt.y}) `;
+    DragTarget.setAttributeNS(null, 'transform', `${transSnap} ${myScale}`);
+    //TransList.appendItem(TransformRequestObj);
+//TODO still see about consolidation
+    //TransList.consolidate();
+    
+    
+    // myCirc = myCirc.cloneNode(true);
+    // pt.x = e.clientX;
+    // pt.y = e.clientY;
+    // // The cursor point, translated into svg coordinates
+    // cursorpt = pt.matrixTransform(svg.getScreenCTM().inverse());
+    // console.log("(cursorpt.x:" + cursorpt.x + ", cursorpt.y:" + cursorpt.y + ")");
+    // let nextPos = findNextGridPosition(cursorpt.x, cursorpt.y);
+    // myCirc.setAttributeNS(null, 'cx', nextPos.x);
+    // myCirc.setAttributeNS(null, 'cy', nextPos.y);
+    // svg.appendChild(myCirc);
 
 }
 
-function oMousePos(elmt, evt) {
-    let ClientRect = elmt.getBoundingClientRect();
-              return { 
-              x: Math.round(evt.clientX - ClientRect.left),
-              y: Math.round(evt.clientY - ClientRect.top)
-    }
-}
+// function oMousePos(elmt, evt) {
+//     let ClientRect = elmt.getBoundingClientRect();
+//               return { 
+//               x: Math.round(evt.clientX - ClientRect.left),
+//               y: Math.round(evt.clientY - ClientRect.top)
+//     }
+// }
 
 /************* just for testing *******************
 **/
 let myCirc = document.createElementNS(svgns, 'circle');
 //myCirc.setAttributeNS(null, 'cx', centerOfCenterHex.x);
 //myCirc.setAttributeNS(null, 'cy', centerOfCenterHex.y);
-myCirc.setAttributeNS(null, 'cx', SIDE*gridStepsInX*4+SIDE);
-myCirc.setAttributeNS(null, 'cy', SIDE*gridStepsInY*5);
-myCirc.setAttributeNS(null, 'style', 'draggable');
-console.log(`origCircle.x: ${SIDE*gridStepsInX*4+SIDE}, origCircle.y: ${SIDE*gridStepsInY*5}`);
-myCirc.setAttributeNS(null, 'r', 10);
+myCirc.setAttributeNS(null, 'cx', 0);
+myCirc.setAttributeNS(null, 'cy', 0);
+//myCirc.setAttributeNS(null, 'style', 'draggable');
+console.log(`origCircle.x: ${SIDE * gridStepsInX * 4 + SIDE}, origCircle.y: ${SIDE * gridStepsInY * 5}`);
+myCirc.setAttributeNS(null, 'r', 0.5);
+myCirc.setAttributeNS(null, 'transform', `translate(${SIDE * gridStepsInX * 4 + SIDE},${SIDE * gridStepsInY * 5}) ${myScale}`);
+console.log(`orig translate of circle(${SIDE * gridStepsInX * 4 + SIDE},${SIDE * gridStepsInY * 5})`);
 myCirc.setAttributeNS(null, 'fill', "red");
-myCirc.addEventListener('click', function (event) { evMouseDown(event) });
+//myCirc.addEventListener('click', function (event) { evMouseDown(event) });
 //myCirc.addEventListener('mousemove', function (event) { evMouseMove(event) });
 //myCirc.addEventListener('mouseup', function (event) { evMouseUp(event) });
 svg.appendChild(myCirc);
@@ -157,21 +206,21 @@ function createSvg() {
 
 function createTurtle(color, stepsInX, stepsInY, stepsIn60Deg, invert) {
     // turtle vectors
-    let turtVectors = 
-    [   /* 1  */{x: 0, y: 0},
-        /* 2  */{x: 3 / 4, y: sqrt3 / 4},
-        /* 3  */{x: 1 / 4, y: -sqrt3 / 4},
-        /* 4  */{x: 1 / 2, y: 0},
-        /* 5  */{x: 0, y: -sqrt3 / 2},
-        /* 6  */{x: -3 / 4, y: -sqrt3 / 4},
-        /* 7  */{x: -3 / 4, y: -sqrt3 / 4},
-        /* 8  */{x: -3 / 4, y: sqrt3 / 4},
-        /* 9  */{x: -1 / 4, y: -sqrt3 / 4},
-        /* 10 */{x: -1 / 2, y: 0},
-        /* 11 */{x: 0, y: sqrt3 / 2},
-        /* 12 */{x: 3 / 4, y: sqrt3 / 4},
-        /* 13 */{x: -1 / 4, y: sqrt3 / 4},
-        /* 14 */{x: 1 / 4, y: sqrt3 / 4}
+    let turtVectors =
+        [   /* 1  */{ x: 0, y: 0 },
+        /* 2  */{ x: 3 / 4, y: sqrt3 / 4 },
+        /* 3  */{ x: 1 / 4, y: -sqrt3 / 4 },
+        /* 4  */{ x: 1 / 2, y: 0 },
+        /* 5  */{ x: 0, y: -sqrt3 / 2 },
+        /* 6  */{ x: -3 / 4, y: -sqrt3 / 4 },
+        /* 7  */{ x: -3 / 4, y: -sqrt3 / 4 },
+        /* 8  */{ x: -3 / 4, y: sqrt3 / 4 },
+        /* 9  */{ x: -1 / 4, y: -sqrt3 / 4 },
+        /* 10 */{ x: -1 / 2, y: 0 },
+        /* 11 */{ x: 0, y: sqrt3 / 2 },
+        /* 12 */{ x: 3 / 4, y: sqrt3 / 4 },
+        /* 13 */{ x: -1 / 4, y: sqrt3 / 4 },
+        /* 14 */{ x: 1 / 4, y: sqrt3 / 4 }
         ];
 
     // add the turtle vectors to get the full path of the turtle polygon
@@ -181,7 +230,7 @@ function createTurtle(color, stepsInX, stepsInY, stepsIn60Deg, invert) {
         tempsY += turtVectors[i].y;
         turtPointsUnitary += ` ${tempsX},${tempsY}`;
     }
-    
+
     let turtPoly = document.createElementNS(svgns, 'polygon');
     turtPoly.setAttributeNS(null, 'fill', color);
     turtPoly.setAttributeNS(null, 'fill-opacity', "0.5");
@@ -194,13 +243,13 @@ function createTurtle(color, stepsInX, stepsInY, stepsIn60Deg, invert) {
     //turtPoly.addEventListener('mousedown', function (event) { evMouseDown(event) });
     //turtPoly.addEventListener('mousemove', function (event) { evMouseMove(event) });
     //turtPoly.addEventListener('mouseup', function (event) { evMouseUo(event) });
-    
+
     return turtPoly;
 }
 
 
 
-function createArbitraryTransform(stepsInX=0, stepsInY=0, stepsIn60Deg=0, invert=1) {
+function createArbitraryTransform(stepsInX = 0, stepsInY = 0, stepsIn60Deg = 0, invert = 1) {
     // stepsInX=stepsInX||0;
     // stepsInY=stepsInY||0;
     // stepsIn60Deg=stepsIn60Deg||0;
@@ -219,7 +268,9 @@ function createRectforPattern() {
     myRect.setAttributeNS(null, 'y', "0");
     myRect.setAttributeNS(null, 'stroke', "black");
     myRect.setAttributeNS(null, 'fill', "url(#hexes)");
+    myRect.setAttributeNS(null, 'id', 'background'); 
     svg.appendChild(myRect);
+    return myRect;
 }
 
 function addHexesToPattern() {
@@ -250,13 +301,13 @@ function addHexesToPattern() {
 function addKitesToPattern() {
     let myPoints = `\
     ${0},${0} \
-    ${3/4},${sqrt3/4} \
+    ${3 / 4},${sqrt3 / 4} \
     ${1},${0} \
-    ${3/4},${-sqrt3/4}`;
+    ${3 / 4},${-sqrt3 / 4}`;
 
     for (let theta = 0; theta < 360; theta += 60) {
         let myRotate = `rotate(${theta}, ${0}, ${0})`;
-        
+
         //create center kite
         let myPoly = document.createElementNS(svgns, 'polygon');
         myPoly.setAttributeNS(null, 'fill', "none");
@@ -317,30 +368,34 @@ function findCenterOfCenterHex() {
     let y = 0;
     let yminusOne = 0;
     //SIDE*sqrt3/2 is offset from 0,0; sqrt3*SIDE is the step in y direction
-    for (y = SIDE * sqrt3/2; y < midSizeY; y = y + (sqrt3 * SIDE)) yminusOne = y;    
+    for (y = SIDE * sqrt3 / 2; y < midSizeY; y = y + (sqrt3 * SIDE)) yminusOne = y;
     let diffDown = y - midSizeY;
     let diffUp = midSizeY - yminusOne;
     if (diffDown > diffUp) {
         //the upper side, yminusOne is the closest to the center
         y = yminusOne;
     }
-    return {x:x, y:y};
+    return { x: x, y: y };
 }
 
 function findNextGridPosition(x, y) {
+    
+    console.log(`origX: ${x}, origY: ${y}`);
     //original pos, top left
-    let nextX = SIDE, prevNextX=0;
-    let nextY = SIDE * sqrt3 / 2, prevNextY=0;
+    let nextX = SIDE, prevNextX = 0;
+    let nextY = SIDE * sqrt3 / 2, prevNextY = 0;
 
+    //console.log(`SIDE: ${SIDE}, origY: ${y}`);
+    //console.log(`nextX: ${nextX}, nextY: ${nextY}`);
     //iterate until find the closest position in the grid
     while (true) {
         if (nextX < x) {
             prevNextX = nextX;
-            nextX += gridStepsInX*SIDE;
+            nextX += gridStepsInX * SIDE;
         }
         if (nextY < y) {
             prevNextY = nextY;
-            nextY += gridStepsInY*SIDE;
+            nextY += gridStepsInY * SIDE;
         }
         if (nextX >= x && nextY >= y) {
             break;
@@ -358,5 +413,5 @@ function findNextGridPosition(x, y) {
         nextY = prevNextY;
     }
     console.log(`nextX: ${nextX}, nextY: ${nextY}`);
-    return {x:nextX, y:nextY};
+    return { x: nextX, y: nextY };
 }
